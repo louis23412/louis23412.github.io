98bcfdee24a74ef3a76386263de55f2062e54bf9
Louis <louis23412@gmail.com>
2025-05-02 17:45:27 +0200

    update info

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const projects = [
     {
-      title: "RESTful API Service",
-      description: "A scalable and secure API built with Node.js and Express, featuring authentication and database integration.",
-      image: "https://via.placeholder.com/300x200",
+      title: "Samle Project 1",
+      description: "Coming Soon",
+      image: "https://placehold.co/300x200",
       link: "#"
     },
     {
-      title: "Database Management Tool",
-      description: "A CLI and web dashboard built with PostgreSQL and Sequelize for data modeling, migration, and analytics.",
-      image: "https://via.placeholder.com/300x200",
+      title: "Samle Project 2",
+      description: "Coming Soon",
+      image: "https://placehold.co/300x200",
       link: "#"
     },
     {
-      title: "Authentication System",
-      description: "JWT-based user authentication and role management system with Express and MongoDB.",
-      image: "https://via.placeholder.com/300x200",
+      title: "Samle Project 3",
+      description: "Coming Soon",
+      image: "https://placehold.co/300x200",
       link: "#"
     },
     {
-      title: "Job Queue & Worker",
-      description: "Queue system using Bull and Redis for handling background jobs like emails and notifications.",
-      image: "https://via.placeholder.com/300x200",
+      title: "Samle Project 4",
+      description: "Coming Soon",
+      image: "https://placehold.co/300x200",
       link: "#"
     }
   ];
